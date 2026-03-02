ef2a1ca501e4c67b9b5834e3b13a27ec5d753bed
Joyce Emília Mota <123253666+NegraEmilia71@users.noreply.github.com>
2026-03-02 17:05:14 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,11 @@
+# Configurações do Projeto de Engenharia de Dados
+
+## Caminhos dos arquivos
+ARQUIVO_PRINCIPAL = '/content/drive/MyDrive/ProgramaMaria/planilha_modulo3.xlsx'
+ARQUIVO_PARTE2 = '/content/drive/MyDrive/ProgramaMaria/Planilha_Aula_parte2.xlsx'
+BANCO_DADOS = '/content/drive/MyDrive/ProgramaMaria/status_brasil'
+ARQUIVO_CSV = '/content/drive/MyDrive/ProgramaMaria/analise_dados.csv'
+
+## Parâmetros de análise
+LIMITE_OUTLIERS = 3  # Desvio padrão para identificação de outliers
+NIVEL_CONFIANCA = 0.95  # Nível de confiança para intervalos
